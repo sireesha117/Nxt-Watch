8df7cabcdf4d9f7eeac8602b7f931b98aa7febe1
sireesha117 <pedamallusireesha117@gmail.com>
2025-04-08 13:13:29 +0000

    Solution

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+
+// Replace your code here
+class App extends Component {
+  render() {
+    return <Switch></Switch>
+  }
+}
+
+export default App
