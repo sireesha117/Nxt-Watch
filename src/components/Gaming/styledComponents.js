d47a2f1c7ee3cda082c68cd9784fd525c84ae8c1
sireesha117 <pedamallusireesha117@gmail.com>
2025-04-13 12:30:33 +0000

    djhehd

---
+++ src/components/Gaming/styledComponents.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+
+export const Divide = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+export const Banner = styled.div`
+  background-image: url('https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png');
+  background-size: contain; /* Use 'contain' to ensure the image fits within the container */
+  background-repeat: no-repeat; /* Prevent the image from repeating */
+  //  background-position: right center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  height: 200px;
+  color: ${props => (props.isLight ? 'black' : 'white')};
+`
+export const Right = styled.div`
+  padding: 10px;
+  background-color: ${props => (props.isLight ? ' #ebebeb' : 'black')};
+  color: ${props => (props.isLight ? 'black' : 'white')};
+  width: 100%;
+`
+
+export const CloseIcon = styled.button`
+display:props => props.closeBtn === true ? "none" : "flex";
+
+background-color:transparent;
+border-width:0px;
+`
+export const BannerImg = styled.img`
+  width: 90px;
+  height: 40px;
+`
+export const HomeData = styled.div`
+  font-family: Roboto;
+`
+export const HomeSuccess = styled.ul`
+  display: flex;
+  flex-direction: row; /* Lay cards horizontally */
+  flex-wrap: wrap; /* Wrap cards to the next row if space runs out */
+  justify-content: flex-start; /* Spread cards evenly */
+  list-style-type: none; /* Remove bullets */
+  padding-left: 0px; /* Remove padding */
+  margin: 0px; /* Remove margin */
+`
+export const Ib = styled.p`
+  margin-left: 0px;
+`
+
+export const HomeList = styled.li`
+  width: 220px; /* Set a fixed width for uniform cards */
+  margin: 10px; /* Space between cards */
+  display: flex;
+  flex-direction: column; /* Stack content vertically */
+
+  word-wrap: break-word; /* Prevent long words from overflowing */
+  overflow: hidden; /* Hide overflowing text */
+`
+
+export const InfoRow = styled.div`
+  display: flex;
+  flex-direction: row; /* Align items horizontally */
+  justify-content: space-between; /* Space between the elements */
+  width: 100%; /* Ensure it spans the full width of the card */
+  margin-top: 5px; /* Space from the title or other elements */
+  font-size: 12px; /* Adjust font size for readability */
+  white-space: nowrap; /* Prevent text from wrapping to the next line */
+  overflow: hidden; /* Prevent text overflow */
+  text-overflow: ellipsis; /* Add ellipsis for overflowing text */
+`
+
+export const HomeImg = styled.img`
+  height: 100px;
+  width: 200px;
+`
+export const HeadingHome = styled.h1`
+  font-size: 16px; /* Make the font size small but readable */
+  margin: 5px 0; /* Add margin for spacing */
+  word-wrap: break-word; /* Ensure long titles break into multiple lines */
+  max-width: 200px; /* Restrict the width to fit the card layout */
+  text-align: left; /* Align text appropriately */
+`
--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,13 +1,170 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Link} from 'react-router-dom'
+import {IoIosClose} from 'react-icons/io'
+import {CiSearch} from 'react-icons/ci'
+import {formatDistanceToNow} from 'date-fns'
 import Header from '../Header'
 import SideBar from '../SideBar'
+import {
+  Divide,
+  Banner,
+  InfoRow,
+  Ib,
+  CloseIcon,
+  HomeImg,
+  HeadingHome,
+  BannerImg,
+  Right,
+  HomeData,
+  HomeSuccess,
+  HomeList,
+} from './styledComponents'
+import WatchContext from '../WatchContext'
 
-const Trending = () => (
-  <div>
-    <Header />
-    <div>
-      <SideBar />
-      <p>hello trend</p>
-    </div>
-  </div>
-)
+const apiSts = {
+  initial: 'INITIAL',
+  inProgress: 'INPROGRESS',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
+class Trending extends Component {
+  state = {
+    close: false,
+
+    apiStsData: apiSts.initial,
+    homeData: [],
+  }
+
+  componentDidMount() {
+    this.getHomeData()
+  }
+
+  onCloseIcon = () => {
+    this.setState({close: true})
+  }
+
+  getFormattedDate = date => {
+    const rawOutput = formatDistanceToNow(new Date(date), {addSuffix: true})
+
+    return rawOutput.replace(/almost |about /g, '')
+  }
+
+  getHomeData = async () => {
+    this.setState({apiStsData: apiSts.inProgress})
+    const url = 'https://apis.ccbp.in/videos/trending'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(url, options)
+    if (response.ok === true) {
+      const data = await response.json()
+      const formattedData = data.videos.map(eachItem => ({
+        id: eachItem.id,
+        title: eachItem.title,
+        thumbnailUrl: eachItem.thumbnail_url,
+        channel: {
+          name: eachItem.channel.name,
+          profileImageUrl: eachItem.channel.profile_image_url,
+        },
+        viewCount:
+          eachItem.view_count > 1000
+            ? `${(eachItem.view_count / 1000).toFixed(1)}k`
+            : eachItem.view_count,
+        publishedAt: this.getFormattedDate(eachItem.published_at),
+      }))
+
+      this.setState({homeData: formattedData, apiStsData: apiSts.success})
+    } else {
+      this.setState({apiStsData: apiSts.failure})
+    }
+  }
+
+  renderHomeData = () => {
+    const {apiStsData, homeData} = this.state
+
+    switch (apiStsData) {
+      case apiSts.inProgress:
+        return <p>loading</p>
+
+      case apiSts.success:
+        return (
+          <HomeSuccess>
+            {homeData.map(video => (
+              <Link to={`/videos/${video.id}`}>
+                <HomeList key={video.id}>
+                  <HomeImg src={video.thumbnailUrl} alt={video.title} />
+                  <HeadingHome>{video.title}</HeadingHome>
+                  <Ib>{video.channel.name}</Ib>
+                  <InfoRow>
+                    <span>{video.viewCount} views</span>
+                    <span>{video.publishedAt}</span>
+                  </InfoRow>
+                </HomeList>
+              </Link>
+            ))}
+          </HomeSuccess>
+        )
+
+      case apiSts.failure:
+        return <p>Something went wrong. Please try again.</p> // Show error message for failure case
+
+      default:
+        return null // Handle the initial state or unexpected cases
+    }
+  }
+
+  render() {
+    return (
+      <WatchContext.Consumer>
+        {value => {
+          const {isLightTheme} = value
+          const {close} = this.state
+          return (
+            <div>
+              <Header />
+              <Divide>
+                <SideBar />
+                <Right isLight={isLightTheme}>
+                  <div>
+                    {!close && (
+                      <Banner isLight={isLightTheme}>
+                        <div>
+                          <BannerImg
+                            src={
+                              isLightTheme
+                                ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
+                                : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
+                            }
+                            alt="logo"
+                          />
+                          <p>Buy Nxt Watch Premium prepaid plans with UPI</p>
+                          <button type="button">Get it now</button>
+                        </div>
+                        <CloseIcon onClick={this.onCloseIcon}>
+                          <IoIosClose />
+                        </CloseIcon>
+                      </Banner>
+                    )}
+                  </div>
+                  <div>
+                    <button type="button" onClick={this.getHomeData}>
+                      <CiSearch />
+                    </button>
+                  </div>
+                  <HomeData>{this.renderHomeData()}</HomeData>
+                </Right>
+              </Divide>
+            </div>
+          )
+        }}
+      </WatchContext.Consumer>
+    )
+  }
+}
+
 export default Trending
