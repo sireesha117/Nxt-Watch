6d1f08e8e32f8cbeae0ffae9a4e4fe267eba3cee
sireesha117 <pedamallusireesha117@gmail.com>
2025-04-13 12:22:10 +0000

    djshdjs

--- src/components/SavedVideos/index.js
+++ src/components/SavedVideos/index.js
@@ -1,13 +1,41 @@
 import Header from '../Header'
 import SideBar from '../SideBar'
+import WatchContext from '../WatchContext'
+import {Right, Divide} from './styledComponents'
 
 const SavedVideos = () => (
-  <div>
-    <Header />
-    <div>
-      <SideBar />
-      <p>hello save</p>
-    </div>
-  </div>
+  <WatchContext.Consumer>
+    {value => {
+      const {isLightTheme, savedVideos} = value
+
+      return (
+        <div>
+          <Header />
+          <Divide>
+            <SideBar />
+            <Right isLight={isLightTheme}>
+              <div>
+                <ul>
+                  {savedVideos.map(eachItem => (
+                    <li>
+                      <img src={eachItem.thumbnailUrl} alt="img" />
+                      <div>
+                        <h1>{eachItem.title}</h1>
+                        <p>{eachItem.channel.name}</p>
+                        <div>
+                          <span>{eachItem.viewCount} views</span>
+                          <span>{eachItem.publishedAt}</span>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Right>
+          </Divide>
+        </div>
+      )
+    }}
+  </WatchContext.Consumer>
 )
 export default SavedVideos
