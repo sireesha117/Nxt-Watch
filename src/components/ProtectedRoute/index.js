5ab2ce952f8548af3c0a38911415cf18ef3b39b8
sireesha117 <pedamallusireesha117@gmail.com>
2025-04-22 07:17:59 +0000

    wdhbhewbd

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,15 +1,12 @@
 import {Route, Redirect} from 'react-router-dom'
-import Cookies from 'js-cookie'
+import Cookie from 'js-cookie'
 
-const ProtectedRoute = ({component: Component, ...rest}) => {
-  const token = Cookies.get('jwt_token')
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        token ? <Component {...props} /> : <Redirect to="/login" />
-      }
-    />
-  )
+const ProtectedRoute = props => {
+  const token = Cookie.get('jwt_token')
+
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
 }
 export default ProtectedRoute
